63011186a50471e978ca0fec8cca7180337316d7
florian dahlitz <f2dahlitz@freenet.de>
2017-04-23 17:53:26 +0200

    fixed 'two same type in a row'-test

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -6,23 +6,57 @@ import re
 
 def tokenize(function: str) -> dict:
     operators = ["+", "-", "*", "/", "^"]
+    r = 0
     result = {}
+    intermediate_result1 = [0,]
+    intermediate_result2 = []
 
     # remove spaces
     function = re.sub("\s", "", function)
 
-    for i, char in enumerate(function):
+    for char in function:
+        # dedicate a type to each value (char)
+        # and allocate it to intermediate_result2
         if char.isdigit():
-            result[i] = ("Literal", char)
+            intermediate_result2.append("Literal")
+            intermediate_result2.append(char)
         elif char.isalpha():
-            result[i] = ("Variable", char)
+            intermediate_result2.append("Variable")
+            intermediate_result2.append(char)
         elif char in operators:
-            result[i] = ("Operator", char)
+            intermediate_result2.append("Operator")
+            intermediate_result2.append(char)
         elif char == "(":
-            result[i] = ("Left Parenthesis", char)
+            intermediate_result2.append("Left Parenthesis")
+            intermediate_result2.append(char)
         elif char == ")":
-            result[i] = ("Right Parenthesis", char)
+            intermediate_result2.append("Right Parenthesis")
+            intermediate_result2.append(char)
+        
+        # checks if values of same type occur in a row
+        # and have to be put together
+        # e.g. '2' and '3' have to be handled as '23'
+        # therefore intermediate_result1 exists
+        # if they don't match put intermediate_result1 into
+        # finished result
+        if intermediate_result1[0] == intermediate_result2[0]:
+            intermediate_result1[1] += intermediate_result2[1]
+            intermediate_result2 = []
+        elif intermediate_result1[0] == 0:
+            intermediate_result1 = []
+            intermediate_result1.append(intermediate_result2[0])
+            intermediate_result1.append(intermediate_result2[1])
+            intermediate_result2 = []
+        else:
+            result[r] = (intermediate_result1[0], intermediate_result1[1])
+            intermediate_result1 = []
+            intermediate_result1.append(intermediate_result2[0])
+            intermediate_result1.append(intermediate_result2[1])
+            intermediate_result2 = []
+            r += 1
+    
+    # append last type and value to result
+    if intermediate_result1 != []:
+        result[r] = (intermediate_result1[0], intermediate_result1[1])
     
     return result
-
-print(tokenize("2x^2+4x-3"))
